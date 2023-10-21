5b540accd9f376e37edffa5e5eaa651209e31971
jaypopdev <jaypopdev@proton.me>
2023-10-21 01:44:35 +0100

    font

--- vim_conf/nvim/init.lua
+++ vim_conf/nvim/init.lua
@@ -109,7 +109,7 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'rose-pine',
         component_separators = '|',
         section_separators = '',
