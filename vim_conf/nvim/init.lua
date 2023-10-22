191b5d517c1a7e654348728c1749dde49f51a3ed
jaypopdev <jaypopdev@proton.me>
2023-10-22 02:38:05 +0100

    fix colours and refactor tmux config

--- vim_conf/nvim/init.lua
+++ vim_conf/nvim/init.lua
@@ -225,13 +225,13 @@ vim.keymap.set("n", "<C-4>", function() ui.nav_file(4) end)
 -- set up git worktree in telescope
 --require("telescope").load_extension("git_worktree")
 -- Keybinding to -create a worktree
--- vim.api.nvim_set_keymap('n', '<leader>gc', [[:lua require("git-worktree").create_worktree()<CR>]],
+-- vim.api.nvim_set_keymap('n', '<leader>wc', [[:lua require("git-worktree").create_worktree()<CR>]],
 --  { noremap = true, silent = true })
 -- Keybinding to switch to an existing worktree
--- vim.api.nvim_set_keymap('n', '<leader>gs', [[:lua require("git-worktree").switch_worktree()<CR>]],
+-- vim.api.nvim_set_keymap('n', '<leader>ws', [[:lua require("git-worktree").switch_worktree()<CR>]],
 --  { noremap = true, silent = true })
 -- Keybinding to delete an existing worktree
---vim.api.nvim_set_keymap('n', '<leader>gd', [[:lua require("git-worktree").delete_worktree()<CR>]],
+--vim.api.nvim_set_keymap('n', '<leader>wd', [[:lua require("git-worktree").delete_worktree()<CR>]],
 -- { noremap = true, silent = true })
 
 -- Clear the background color for Normal mode
@@ -342,22 +342,12 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
--- custom func to search and exlude .html files
-vim.keymap.set('n', '<leader>sr', function()
-  require('telescope.builtin').find_files({
-    prompt_title = '[S]earch [R]esume',
-    find_command = { 'rg', '--files', '--hidden', '--type', 'not:text', '--ignore-file', '!.html' },
-    hidden = true,
-  })
-end)
-
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
